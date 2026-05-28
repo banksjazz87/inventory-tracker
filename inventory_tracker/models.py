6e6fabbb33c7f2a4f4208bf0e39d2d09b442a5d1
Chris <banksjazz87@gmail.com>
2026-05-28 12:50:12 -0400

    revision: revised my models

--- inventory_tracker/models.py
+++ inventory_tracker/models.py
@@ -1,30 +1,53 @@
 from django.db import models
 
-class Ingredients(models.Model):
+class Ingredient(models.Model):
+    COOKING_UNITS = {
+        "tsp": "Teaspoon",
+        "tbsp": "Tablespoon",
+        "fl oz": "Fluid Ounce",
+        "c": "Cup",
+        "pt": "Pint",
+        "gal": "Gallon",
+        "ml": "Milliliter",
+        "l": "Liter",
+        "g": "Gram",
+        "kg": "Kilogram",
+        "oz": "Ounce",
+        "lb": "Pound",
+        "mg": "Milligram",
+        "cn": "Can",
+        "pkg": "Package"
+    }
     name = models.CharField(max_length=250)
     quantity = models.IntegerField()
-    unit_name = models.CharField(max_length=75)
+    unit_name = models.CharField(max_length=5, choices=COOKING_UNITS)
     price_per_unit = models.DecimalField(max_digits=8, decimal_places=2)
 
     def __str__(self):
         return f"\n name: {self.name}\n quantity: {self.quantity}\n unit_name: {self.unit_name}\n price_per_unit: {self.price_per_unit}"
 
 
-class Menu_Items(models.Model):
+class MenuItem(models.Model):
     name = models.CharField(max_length=250)
     price = models.DecimalField(max_digits=8, decimal_places=2)
-    #Pass in the name of the of the ingredient as a comma separated list.
-    ingredients = models.TextField()
 
     def __str__(self):
-        return f"\n name: {self.name}\nprice: {self.price}\n ingredients: {self.ingredients}"
+        return f"\n name: {self.name}\nprice: {self.price}"
 
-class Purchases(models.Model):
-    menu_item = models.ForeignKey(Menu_Items, on_delete=models.CASCADE)
-    quantity_sold = models.IntegerField()
-    purchase_time = models.DateTimeField()
-    total = models.DecimalField(max_digits=10, decimal_places=2)
+
+class RecipeRequirement(models.Model):
+    menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    quantity = models.DecimalField(max_digits=6, decimal_places=2)
 
     def __str__(self):
-        return f"\n menu_item: {self.menu_item}\n quantity_sold: {self.quantity_sold}\n purchase_time: {self.purchase_time}\n total: {self.total}"
+        return f"\n menu_item: {self.menu_item} \n ingredient: {self.ingredient} \n quantity: {self.quantity}"
+
+
+class Purchase(models.Model):
+    menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    time_stamp = models.DateTimeField()
+
+    def __str__(self):
+        return f"\n menu_item: {self.menu_item}\n time_stamp: {self.time_stamp}"
 
